bd27ed84585a76a7a43da61a66dda188bc6ff630
Christian Johnson <cartian4@gmail.com>
2022-08-10 21:57:55 -0400

    Mess with array

--- 8-10-22/problem.c
+++ 8-10-22/problem.c
@@ -1,34 +1,37 @@
 #include <stdio.h>
 
-int possible_numbers = 0;
-
 int main (void)
 {
-    int k = 0;
+    int possible_numbers = 0;
+    int k;
     int size;
+    int i;
 
     printf("Enter the size of the array:\n");
-    scanf("%d", &size);
+    scanf("%i", &size);
 
     int array[size];
 
-    printf("Enter %d numbers:\n", size);
+    printf("Enter %i numbers:\n", size);
     for (int i = 0; i < size; i++)
     {
-        scanf_s("%d", array[size]);
+        scanf("%i", &array[size]);
     }
 
+    printf("The current array is: %i\n", array[i]);
+
+
     printf("Enter the master number:\n");
-    scanf("%d", &k);
+    scanf("%i", &k);
 
     //Search array and compare
-    for (int i = 0; i < size; i++)
+    for (int j = 0; j < size; j++)
     {
         int subtracted_number = 0;
 
         subtracted_number = k - array[i];
 
-        for (int j = 0; j < size; j++)
+        for (int l = 0; l < size; l++)
         {
             if (subtracted_number == array[i])
             {
