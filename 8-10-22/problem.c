da07cc53f3293aab912e57b40c91808deabcb7bb
Christian Johnson <cartian4@gmail.com>
2022-08-10 21:31:43 -0400

    Add user input for array and integer k

--- 8-10-22/problem.c
+++ 8-10-22/problem.c
@@ -2,20 +2,37 @@
 
 int possible_numbers = 0;
 
-int main (int argc, int argv[])
+int main (void)
 {
+    int k;
+    int size;
+
+    printf("Enter the size of the array:\n");
+    scanf("%d", &size);
+
+    int array[size];
+
+    printf("Enter %d numbers:\n", size);
+    for (int i = 0; i < size; i++)
+    {
+        scanf_s("%d", array[size]);
+    }
+
+    printf("Enter the master number:\n");
+    scanf("%d", &k);
+
     //Search array and compare
-    for (int i = 0; i < arraylength; i++)
+    for (int i = 0; i < size; i++)
     {
         int subtracted_number = 0;
 
         subtracted_number = k - array[i];
 
-        for (int j = 0; j < arraylength; j++)
+        for (int j = 0; j < size; j++)
         {
             if (subtracted_number == array[i])
             {
-                possible_numbers++
+                possible_numbers++;
             }
         }
     }
@@ -30,7 +47,4 @@ int main (int argc, int argv[])
         printf("False\n");
         return 1;
     }
-
-
-
 }
